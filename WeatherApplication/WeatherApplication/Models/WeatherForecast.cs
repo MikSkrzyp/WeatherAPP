3b8f57291429d32f8794646d43c5ec95ca816e21
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2024-01-08 20:27:05 +0100

    adding validation to input tag from server side

--- WeatherApplication/WeatherApplication/Models/WeatherForecast.cs
+++ WeatherApplication/WeatherApplication/Models/WeatherForecast.cs
@@ -4,10 +4,7 @@ namespace WeatherApplication.Models
 {
     public class WeatherForecast
     {
-
-        [Required(ErrorMessage = "City is required")]
-        [StringLength(20, ErrorMessage = "City should be at most 20 characters")]
-        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "City should not contain numbers and special characters (except space)")]
+       
         public string City { get; set; }
         public string Temperature { get; set; }
         public string Description { get; set; }
