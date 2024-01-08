34f553ffcaa474a8546b6e07dc7711f05cf607e4
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2024-01-08 17:07:03 +0100

    Adding error handling

--- WeatherApplication/WeatherApplication/Controllers/WeatherController.cs
+++ WeatherApplication/WeatherApplication/Controllers/WeatherController.cs
@@ -30,7 +30,7 @@ namespace WeatherApplication.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(string city)
         {
-            var apiKey = "InsertHereYouApiKey";
+            var apiKey = "9b61e791ac55978d74b4c0372ad11745";
             var request = new HttpRequestMessage(HttpMethod.Get,
                 $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric");
             var client = _clientFactory.CreateClient();
@@ -64,6 +64,8 @@ namespace WeatherApplication.Controllers
             }
             else
             {
+                
+                TempData["ErrorMessage"] = "Failed to retrieve weather data. Please try again.";
                 return View(new Tuple<List<WeatherData>, WeatherForecast>(_dbContext.WeatherData.ToList(), new WeatherForecast()));
             }
         }
--- WeatherApplication/WeatherApplication/Models/WeatherForecast.cs
+++ WeatherApplication/WeatherApplication/Models/WeatherForecast.cs
@@ -2,6 +2,8 @@
 {
     public class WeatherForecast
     {
+
+
         public string City { get; set; }
         public string Temperature { get; set; }
         public string Description { get; set; }
